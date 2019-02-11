9cc2bab919aa12b544418c12206d9cc2257352c2
Matthew <mattgray1986@icloud.com>
2019-02-10 23:06:19 -0600

    Refactor

--- src/main/java/com/mg1986/filesyncer/FileSyncer.java
+++ src/main/java/com/mg1986/filesyncer/FileSyncer.java
@@ -1,16 +1,20 @@
-package com.mg1986.FileSyncer;
+package com.mg1986.filesyncer;
 
-import java.io.*;
-import java.nio.file.*;
+import java.io.File;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
-import static java.nio.file.StandardCopyOption.*;
+import org.apache.commons.io.*;
 
 public class FileSyncer {
 
     private final static String separator = "----------------------------------";
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
         if (args.length == 2) {
             String sourceURL = args[0];
@@ -19,12 +23,25 @@ public class FileSyncer {
             File targetDirectory = new File(targetURL);
 
             if (sourceDirectory.isDirectory() && targetDirectory.isDirectory()) {
+
+                String beginTimeStamp = new SimpleDateFormat("yyyy/MM/dd k:m").format(Calendar.getInstance().getTime());
+                System.out.println("File sync began at: " + beginTimeStamp + "\n" + separator);
+
                 desync(targetURL, sourceURL);
                 sync(sourceURL, targetURL);
+
+                String endTimeStamp = new SimpleDateFormat("yyyy/MM/dd k:m").format(Calendar.getInstance().getTime());
+                System.out.println("File sync ended at: " + endTimeStamp + "\n" + separator);
+
+            } else {
+                System.out.println("FileSyncer requires valid source and target folder paths as program inputs");
             }
+        } else {
+            System.out.println("FileSyncer requires valid source and target folder paths as program inputs");
         }
     }
 
+    // Adds/replaces files/directories from source to target
     private static void sync(String sourceDirectory, String targetDirectory) {
 
         File folder = new File(sourceDirectory);
@@ -34,7 +51,7 @@ public class FileSyncer {
                 String targetURL = source.getAbsolutePath().replace(sourceDirectory, targetDirectory);
                 File target = new File(targetURL);
                 if (source.isDirectory()) {
-                    if (target.isDirectory()) {
+                    if (target.isDirectory() && target.exists()) {
                         sync(source.getAbsolutePath(), target.getAbsolutePath());
                     } else {
                         Files.copy(source.toPath(), target.toPath(), REPLACE_EXISTING);
@@ -45,22 +62,20 @@ public class FileSyncer {
                         Files.copy(source.toPath(), target.toPath(), REPLACE_EXISTING);
                         System.out.println("File synced: " + target);
                     } else {
-                        byte[] f1 = Files.readAllBytes(source.toPath());
-                        byte[] f2 = Files.readAllBytes(target.toPath());
-                        if (!Arrays.equals(f1, f2)) {
+                        if (!filesAreIdentical(source, target)) {
                             Files.copy(source.toPath(), target.toPath(), REPLACE_EXISTING);
                             System.out.println("File synced: " + target);
                         }
                     }
                 }
             } catch (IOException ioe) {
-                ioe.printStackTrace(System.out);
                 String targetURL = source.getAbsolutePath().replace(sourceDirectory, targetDirectory);
                 System.out.println("Sync failed: " + targetURL);
             }
         }
     }
 
+    // Removes files/directories from target if they don't exist in source
     private static void desync(String targetDirectory, String sourceDirectory) {
 
         File folder = new File(targetDirectory);
@@ -70,13 +85,32 @@ public class FileSyncer {
                 String sourceURL = target.getAbsolutePath().replace(targetDirectory, sourceDirectory);
                 File source = new File(sourceURL);
                 if (target.exists() && !source.exists()) {
+                    System.out.println("File/Directory removed: " + target.getAbsolutePath());
                     Files.deleteIfExists(target.toPath());
                 } else if (target.isDirectory()){
                     desync(target.getAbsolutePath(), source.getAbsolutePath());
                 }
             } catch (IOException ioe){
-                System.out.println("Sync failed: " + target.getAbsolutePath());
+                System.out.println("Desync failed: " + target.getAbsolutePath());
             }
         }
+    }
+
+    // Returns true if two files are identical
+    private static boolean filesAreIdentical(File file1, File file2) {
+
+        boolean identical = false;
+
+        try {
+            if (file1.length() != file2.length()) {
+                identical = false;
+            } else if (FileUtils.contentEquals(file1, file2)) {
+                identical = true;
+            }
+        } catch (IOException ioe) {
+            System.out.println("IO Error");
+        }
+
+        return identical;
     }
 }
